bfa3234a800ee0caed3777787c9c7216a804647c
junjianglin <junjianglin.cs@gmail.com>
2015-08-23 13:04:53 -0400

    readin integer, simple calculation

---
+++ IntroductionToProgramming/E8.py
@@ -0,0 +1,4 @@
+n_w = input("please input the number of widgets")
+n_g = input("please input the number of gizmos")
+sum = n_w*75 + n_g*112
+print "the total weight of your widgets and gizmos is: ", sum
